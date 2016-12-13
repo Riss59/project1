9be3e3009b371edccec5fbc2418f7eab71732012
Riss59 <wojtek.paturaj@gmail.com>
2016-12-13 14:59:50 +0100

    Update FRNN.py

--- FRNN.py
+++ FRNN.py
@@ -1,11 +1,10 @@
 #!/usr/bin/env python
 import numpy as np
 import pandas as pd
-# import time as tm
 from sklearn import preprocessing
 from sklearn.datasets import load_iris
 
-def odl(x,newdata_ii):
+def odl(x,newdata_ii): 
     return(np.sqrt(sum(x.subtract(newdata_ii).apply(float)**2)));
 
 def get_kneighbours(decision_table,newdata_ii,k,method_type):
@@ -16,13 +15,7 @@ def get_kneighbours(decision_table,newdata_ii,k,method_type):
     nearest_dt.dropna()
     return(nearest_dt)
 
-def calc_membership(nearest_dt,num_class,type_="gradual"):
-    num_Class=pd.DataFrame
-    array_w=[]
-    for i in range(num_class):
-        array_w.append(nearest_dt[nearest_dt.iloc[:,(nearest_dt.shape[1]-1-1)]==i].shape[0])
-    num_Class=pd.DataFrame(array_w).T
-
+def calc_membership(nearest_dt,num_class,num_Class,type_="gradual"):
     #numClass[1, i] <- nrow(nearest.dt[which(nearest.dt[,(ncol(nearest.dt) - 1)] == i), ,drop = FALSE])
     ## calculate membership of each class based on Keller et. al.'s technique.
     miu_class=pd.DataFrame()
@@ -38,7 +31,7 @@ def calc_membership(nearest_dt,num_class,type_="gradual"):
                     array_miu_class.append(0.51+(num_Class.iloc[0,int(nearest_dt.iloc[i,nearest_dt.shape[1]-1-1])]/float(nearest_dt.shape[0])*0.49))
                 elif(type_=="crisp"):
                     array_miu_class.append(1)
-
+        
             else:
                 if(type_=="gradual"):
                     array_miu_class.append(num_Class.iloc[0,j]/float(nearest_dt.shape[0])*0.49)
@@ -46,18 +39,18 @@ def calc_membership(nearest_dt,num_class,type_="gradual"):
                     array_miu_class.append(0)
         miu_class=miu_class.append(pd.DataFrame(array_miu_class).T)
         #suma miu class musi być równa 1
-    return(miu_class)
+    return(miu_class)        
 
 
 # Funkcja Pomiar podobieństwa
 # @param nearest_dt matrix of data nearest neighobur
-# @control a dictionary of parameters
-def calc_similiarity_degree(nearest_dt,control):
-    num_class=control["num_class"]
+# @control a dictionary of parameters 
+def calc_similiarity_degree(nearest_dt,control,num_Classs):
+    num_clas=control["num_class"]
     m=control["m"]
     # calculate membership of newdata to each class
     ## miu.class is a matrix (k, num.class) where k is number of neighbor
-    miu_clas=calc_membership(nearest_dt=nearest_dt,num_class=num_class,type_="gradual")
+    miu_clas=calc_membership(nearest_dt=nearest_dt,num_class=num_class,num_Class=num_Classs,type_="gradual")
     ## calculate sum on denominator of the similarity eq.
     sum_denominator=sum(1/nearest_dt.iloc[:,nearest_dt.shape[1]-1]**(2/(m-1)))
     ## calculate membership function of class for determining class of newdata
@@ -80,6 +73,11 @@ def C_FRNN_O_FRST(decision_table,newdata,control):
     dec_table=pd.concat([decsion_table_norm,result_decision_table],axis=1)
     num_inputvar=float(dec_table.shape[1]) - 1
     num_instances=float(dec_table.shape[0])
+    num_Class=pd.DataFrame
+    array_w=[]
+    for i in range(num_class):
+        array_w.append(dec_table[dec_table.iloc[:,(dec_table.shape[1]-1)]==i].shape[0]) 
+    num_Class=pd.DataFrame(array_w).T
     res_class=[]
     for i in range(newdata.shape[0]):
         num_instances=float(num_instances)
@@ -88,56 +86,51 @@ def C_FRNN_O_FRST(decision_table,newdata,control):
             distance.append(np.power(np.sum(np.power(dec_table.iloc[j,:-1]-newdata.iloc[i,:],2)),0.5))
         k_averDist=1/((1/num_instances)*np.sum(np.power(distance,2/(m-1))))
         ## calculate and get K-nearest neighbor (in this case, K = num.instances)
-        nearest_dt=get_kneighbours(dec_table,newdata.iloc[i,:],num_instances,"cos")
+        #nearest_dt=get_kneighbours(dec_table,newdata.iloc[i,:],num_instances,"cos")
+        nearest_dt=pd.concat([dec_table,pd.DataFrame(distance)],axis=1)
         nearest_dt[nearest_dt.iloc[:,nearest_dt.shape[1]-1]==0]=0.00001
         ## calculate membership of each class based on nearest.dt (in this case: all training data)
-        miu=calc_similiarity_degree(nearest_dt,control)
+        miu=calc_similiarity_degree(nearest_dt,control,num_Class)
         ## calculate fuzzy-rough ownership function
         array_tau=[]
         for c in range(num_class):
             temp=0
             for jj in range(int(num_instances)):
-                distance_i=np.power(np.sum(np.power(dec_table.iloc[jj,:-1]-newdata.iloc[i,:],2)),0.5)
+                #distance_i=np.power(np.sum(np.power(dec_table.iloc[jj,:-1]-newdata.iloc[i,:],2)),0.5)
+                distance_i=nearest_dt.iloc[jj,-1]
                 temp=temp+miu[c]/(1+k_averDist*np.power(distance_i,(2/(m-1))))
             array_tau.append((1/float(num_instances))*temp)
         res_class.append(np.argmax(array_tau))
     return_class=pd.DataFrame(res_class)
     return(return_class)
 
+import time
 
-print("start")
-
-# tic=tm.clock()
+print("Init")
+tic=time.clock()
 
 data = load_iris()
 data_x=pd.DataFrame(data.data)
 wynik_x=pd.DataFrame(data.target)
 data=pd.concat([data_x,wynik_x],axis=1)
+decision_table_iris=data.sample(frac=0.7)
+newdata_iris=data.loc[~data.index.isin(decision_table_iris.index)]
+wyniki_newdata=newdata_iris.iloc[:,-1]
+wyniki_newdata.index=range(45)
+newdata_iris=newdata_iris.iloc[:,:-1]
+control={"num_class":3,"m":3,"type_membership":"gradual"}
+toc=time.clock()
+print(toc-tic)
 
-decision_table_iris=data.iloc[range(110),:]
-newdata_iris=data.iloc[range(110,150),:-1]
-wyniki_newdata=data.iloc[range(110,150),4]
-wyniki_newdata.index=range(40)
+tic=time.clock()
+wynikitestow1=C_FRNN_O_FRST(decision_table_iris,newdata_iris,control)
+toc=time.clock()
 
-control={"num_class":3,"m":3,"type_membership":"cos"}
+print("FRNN",toc-tic)
 
-# toc=tm.clock()
-
-# print("Init", toc-tic)
-
-# tic=tm.clock()
+porownanie=pd.concat([pd.DataFrame(wynikitestow1),pd.DataFrame(wyniki_newdata)],axis=1)
 
 
-wynikitestow=C_FRNN_O_FRST(decision_table_iris,newdata_iris,control)
-
-# toc=tm.clock()
-
-# print("FRNN", toc-tic)
-
-# tic=tm.clock()
-porownanie=pd.concat([pd.DataFrame(wynikitestow),pd.DataFrame(wyniki_newdata)],axis=1)
-# toc=tm.clock()
-# print("end", toc-tic)
 
 
 
