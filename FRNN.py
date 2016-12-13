85461c629eec0b809da9cf962e8d6e92f4a17e23
Tomasz Górecki <tomasz.gorecki@ts.fujitsu.com>
2016-12-13 10:54:49 +0100

    Added shebang, made script executable

--- FRNN.py
+++ FRNN.py
@@ -1,9 +1,11 @@
+#!/usr/bin/env python
 import numpy as np
 import pandas as pd
+# import time as tm
 from sklearn import preprocessing
 from sklearn.datasets import load_iris
 
-def odl(x,newdata_ii): 
+def odl(x,newdata_ii):
     return(np.sqrt(sum(x.subtract(newdata_ii).apply(float)**2)));
 
 def get_kneighbours(decision_table,newdata_ii,k,method_type):
@@ -18,9 +20,9 @@ def calc_membership(nearest_dt,num_class,type_="gradual"):
     num_Class=pd.DataFrame
     array_w=[]
     for i in range(num_class):
-        array_w.append(nearest_dt[nearest_dt.iloc[:,(nearest_dt.shape[1]-1-1)]==i].shape[0]) 
+        array_w.append(nearest_dt[nearest_dt.iloc[:,(nearest_dt.shape[1]-1-1)]==i].shape[0])
     num_Class=pd.DataFrame(array_w).T
-    
+
     #numClass[1, i] <- nrow(nearest.dt[which(nearest.dt[,(ncol(nearest.dt) - 1)] == i), ,drop = FALSE])
     ## calculate membership of each class based on Keller et. al.'s technique.
     miu_class=pd.DataFrame()
@@ -36,7 +38,7 @@ def calc_membership(nearest_dt,num_class,type_="gradual"):
                     array_miu_class.append(0.51+(num_Class.iloc[0,int(nearest_dt.iloc[i,nearest_dt.shape[1]-1-1])]/float(nearest_dt.shape[0])*0.49))
                 elif(type_=="crisp"):
                     array_miu_class.append(1)
-        
+
             else:
                 if(type_=="gradual"):
                     array_miu_class.append(num_Class.iloc[0,j]/float(nearest_dt.shape[0])*0.49)
@@ -44,14 +46,14 @@ def calc_membership(nearest_dt,num_class,type_="gradual"):
                     array_miu_class.append(0)
         miu_class=miu_class.append(pd.DataFrame(array_miu_class).T)
         #suma miu class musi być równa 1
-    return(miu_class)        
+    return(miu_class)
 
 
 # Funkcja Pomiar podobieństwa
 # @param nearest_dt matrix of data nearest neighobur
-# @control a dictionary of parameters 
+# @control a dictionary of parameters
 def calc_similiarity_degree(nearest_dt,control):
-    num_clas=control["num_class"]
+    num_class=control["num_class"]
     m=control["m"]
     # calculate membership of newdata to each class
     ## miu.class is a matrix (k, num.class) where k is number of neighbor
@@ -103,18 +105,39 @@ def C_FRNN_O_FRST(decision_table,newdata,control):
     return(return_class)
 
 
+print("start")
+
+# tic=tm.clock()
 
 data = load_iris()
 data_x=pd.DataFrame(data.data)
 wynik_x=pd.DataFrame(data.target)
 data=pd.concat([data_x,wynik_x],axis=1)
+
 decision_table_iris=data.iloc[range(110),:]
 newdata_iris=data.iloc[range(110,150),:-1]
 wyniki_newdata=data.iloc[range(110,150),4]
 wyniki_newdata.index=range(40)
+
 control={"num_class":3,"m":3,"type_membership":"cos"}
+
+# toc=tm.clock()
+
+# print("Init", toc-tic)
+
+# tic=tm.clock()
+
+
 wynikitestow=C_FRNN_O_FRST(decision_table_iris,newdata_iris,control)
+
+# toc=tm.clock()
+
+# print("FRNN", toc-tic)
+
+# tic=tm.clock()
 porownanie=pd.concat([pd.DataFrame(wynikitestow),pd.DataFrame(wyniki_newdata)],axis=1)
+# toc=tm.clock()
+# print("end", toc-tic)
 
 
 
